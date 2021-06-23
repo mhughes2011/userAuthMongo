29b48f8a9bf2a764d821a3f332e03557d8f79fbe
M Hughes <mhughes2011@gmail.com>
2021-06-23 10:33:07 -0700

    added register routes to handle sign up

--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,26 @@ var router = express.Router();
 // GET /
 router.get('/', function(req, res, next) {
   return res.render('index', { title: 'Home' });
-});
+})
 
 // GET /about
 router.get('/about', function(req, res, next) {
   return res.render('about', { title: 'About' });
-});
+})
 
 // GET /contact
 router.get('/contact', function(req, res, next) {
   return res.render('contact', { title: 'Contact' });
-});
+})
+
+// GET /register
+router.get('/register', (req, res, next) => {
+  res.send('Register Today!');
+})
+
+// POST /register
+router.post('/register', (req, res, next) => {
+  res.send('User Created!');
+})
 
 module.exports = router;
